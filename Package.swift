af3b6db6493188e85831442f914a35f1c4c24990
Kazuki Horie <1ao.keld.b@gmail.com>
2022-07-21 04:10:57 +0900

    nendad only

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
             name: "VAMP-SPM",
             targets: [
                 "VAMP",
-                "VAMPMaioAdapter",
-                "Maio",
+//                "VAMPMaioAdapter",
+//                "Maio",
                 "VAMPNendAdapter",
                 "NendAd",
-                "VAMPTapjoyAdapter",
-                "Tapjoy",
-                "VAMPUnityAdsAdapter",
-                "UnityAds",
+//                "VAMPTapjoyAdapter",
+//                "Tapjoy",
+//                "VAMPUnityAdsAdapter",
+//                "UnityAds",
             ]),
     ],
     dependencies: [
@@ -39,16 +39,16 @@ let package = Package(
             url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMP-v4.4.2.zip",
             checksum: "761cdb9334c5e4d1ba435cdca63e0dc5a44951e71bbbfc02b9dc5741faff59d9"
         ),
-        .binaryTarget(
-            name: "VAMPMaioAdapter",
-            url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMPMaioAdapter-v1.6.2.0.zip",
-            checksum: "6c9b7ddee0121f931d6b286e052ebb48cb04ff6f0a34d0da8fc7b9bb461b9782"
-        ),
-        .binaryTarget(
-            name: "Maio",
-            url: "https://github.com/imobile-maio/maio-iOS-SDK/releases/download/v1.6.2/Maio.xcframework.zip",
-            checksum: "d2043ffd543649d39d845ea162d67de21cd1e3ce855c1e11bec2f9b8d671a8cb"
-        ),
+//        .binaryTarget(
+//            name: "VAMPMaioAdapter",
+//            url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMPMaioAdapter-v1.6.2.0.zip",
+//            checksum: "6c9b7ddee0121f931d6b286e052ebb48cb04ff6f0a34d0da8fc7b9bb461b9782"
+//        ),
+//        .binaryTarget(
+//            name: "Maio",
+//            url: "https://github.com/imobile-maio/maio-iOS-SDK/releases/download/v1.6.2/Maio.xcframework.zip",
+//            checksum: "d2043ffd543649d39d845ea162d67de21cd1e3ce855c1e11bec2f9b8d671a8cb"
+//        ),
         .binaryTarget(
             name: "VAMPNendAdapter",
             url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMPNendAdapter-v7.3.0.0.zip",
@@ -62,29 +62,29 @@ let package = Package(
             url: "https://github.com/kazuki-horie-m/NendAd-SPM/raw/main/NendAd.xcframework.zip",
             checksum: "3f5e1af0c55f30822f0f8b6368e272082252f18ae14a6b0340506c55dd75644e"
         ),
-        .binaryTarget(
-            name: "VAMPTapjoyAdapter",
-            url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMPTapjoyAdapter-v12.10.0.0.zip",
-            checksum: "a5e3a36585d26460d674a96ea6fb89855be7e39d84c637f1f24bc30950e3bdcd"
-        ),
-        .binaryTarget(
-            name: "Tapjoy",
+//        .binaryTarget(
+//            name: "VAMPTapjoyAdapter",
+//            url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMPTapjoyAdapter-v12.10.0.0.zip",
+//            checksum: "a5e3a36585d26460d674a96ea6fb89855be7e39d84c637f1f24bc30950e3bdcd"
+//        ),
+//        .binaryTarget(
+//            name: "Tapjoy",
 //            path: "Libraries/Tapjoy.xcframework",
 //            url: "https://sdk.tapjoy.com/releases/12.9.1/TapjoySDK_iOS_v12.9.1.zip",
 //            checksum: "19cd1d1faf2a3fb3d145c2d15a010f5869df8a53e018d68f45e2318539cf5cdb"
-            url: "https://github.com/kazuki-horie-m/Tapjoy-SPM/raw/main/Libraries/Tapjoy.xcframework.zip",
-            checksum: "73d24fe4c9c1d1051c2433698b6639566b9774ff63dcd46598af1d53c465908f"
-        ),
-        .binaryTarget(
-            name: "VAMPUnityAdsAdapter",
-            url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMPUnityAdsAdapter-v4.2.1.0.zip",
-            checksum: "d5f57c8d9365a4af712daf1a78c04b40582c1889ba5c4c1c7c70a976242f3d1b"
-        ),
-        .binaryTarget(
-            name: "UnityAds",
-            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.2.1/UnityAds.zip",
-            checksum: "38beb90ae58b758e37f52d83dbafba0d1087e1b5a93986ca87610cc373862c9e"
-        ),
+//            url: "https://github.com/kazuki-horie-m/Tapjoy-SPM/raw/main/Libraries/Tapjoy.xcframework.zip",
+//            checksum: "73d24fe4c9c1d1051c2433698b6639566b9774ff63dcd46598af1d53c465908f"
+//        ),
+//        .binaryTarget(
+//            name: "VAMPUnityAdsAdapter",
+//            url: "https://d2dylwb3shzel1.cloudfront.net/iOS/VAMPUnityAdsAdapter-v4.2.1.0.zip",
+//            checksum: "d5f57c8d9365a4af712daf1a78c04b40582c1889ba5c4c1c7c70a976242f3d1b"
+//        ),
+//        .binaryTarget(
+//            name: "UnityAds",
+//            url: "https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.2.1/UnityAds.zip",
+//            checksum: "38beb90ae58b758e37f52d83dbafba0d1087e1b5a93986ca87610cc373862c9e"
+//        ),
         .testTarget(
             name: "VAMP-SPMTests",
             dependencies: ["VAMP-SPM"]),
